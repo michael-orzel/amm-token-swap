4aa5bad3530d3d50bd7ccb715fa26fef827bc407
Michael Orzel <hello@michaelorzel.com>
2022-03-22 19:01:36 -0400

    metamask connectivity

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Greeter from '../../artifacts/contracts/Greeter.sol'
+import { ethers } from "ethers"
 
 export default function App() {
+  useEffect(() => {
+    loadBlockchainData();
+  }, [])
+  
+  const loadBlockchainData = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please install MetaMask!");
+        window.location.assign("https://metamask.io/");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
-      <p className='text-green-600 font-bold'>temporary...</p>
+      <p className='text-green-500'>temporary...</p>
     </div>
-  );
+  )
 }
